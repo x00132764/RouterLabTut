65f303947ea1545183a3f5ba3b6baa46f8043b86
Enda Lee <enda.lee@ittdublin.ie>
2016-11-28 10:27:19 +0000

    this time, page2 modified

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Location } from "@angular/common";
+ 
 @Component({
-  selector: 'app-page2',
-  templateUrl: './page2.component.html',
-  styleUrls: ['./page2.component.css']
+    selector: 'app-page2',
+    templateUrl: './page2.component.html',
+    styleUrls: ['./page2.component.css']
 })
-export class Page2Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class Page2Component {
+ 
+    constructor(private location: Location) { }
+ 
+    goBack() {
+        this.location.back();
+    }
+ 
 }
